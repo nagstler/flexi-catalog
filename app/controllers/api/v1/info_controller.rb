55c3c0f63140feafbc9bf6e61dd3ef8a100ea7f0
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2023-04-06 00:58:50 +0530

    Added API versioing scheme & otger routing updates

---
+++ app/controllers/api/v1/info_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module V1
+    class InfoController < ApplicationController
+      def index
+        @info = Rails.cache.fetch('api/v1/info', expires_in: 1.hour) do
+          {
+            status: 'OK',
+            current_time: Time.now.utc.iso8601,
+            environment: Rails.env,
+            version: MyApp::VERSION
+          }
+        end
+
+        render json: @info
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'api/v1/info#index'
   # Defines the root path route ("/")
-  root to: 'info#index'
+  Rails.application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        get 'info', to: 'info#index'
+      end
+    end
+  end
+  
 end
